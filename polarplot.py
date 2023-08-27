23cee85e7ae87a45f9f938b28604b13fdd877533
Andras <agalambos3@gmail.com>
2023-08-27 16:11:39 +0200

    more comments

--- polarplot.py
+++ polarplot.py
@@ -2,9 +2,10 @@ import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 import numpy as np
 
-# Compute areas and colors
 
-data = np.loadtxt("Sand Table Code/thr files/reverse-HEP.txt")
+#load data from thr file
+data = np.loadtxt("Sand Table Code/Dithermaster sisyphus master thr_paths/dither_tri4.thr")
+#reshape data and seperate into theta and r array
 tdata = np.transpose(data)
 theta = tdata[0]
 r = tdata[1]
@@ -12,17 +13,21 @@ color = np.linspace(0,1,len(r))
 
 
 
-
+#create figure with two polar plots side by side
 fig, (ax1, ax2) = plt.subplots(1, 2,subplot_kw={"projection":"polar"})
+
+#polar scatter plot
 splot = ax1.scatter(theta, r,s=2,c=color,cmap="plasma")
 ax1.set_yticks(np.array([]))
 ax1.set_xticks([])
 
+#polar line plot
 pplot = ax2.plot(theta,r,linewidth=.5)[0]
 ax2.set_yticks(np.array([]))
 ax2.set_xticks([])
 
 
+#updating plot for animation
 def update(frame):
     # for each frame, update the data stored on each artist.
     x = theta[:frame]
@@ -35,8 +40,8 @@ def update(frame):
     pplot.set_ydata(r[:frame])
     return (splot,pplot)
     
-
-
-ani = animation.FuncAnimation(fig=fig, func=update, frames=len(r), interval=1)
+is_animated = True
+if is_animated == True:
+    ani = animation.FuncAnimation(fig=fig, func=update, frames=len(r), interval=1)
 
 plt.show()
