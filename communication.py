bb346e78f004db20b078e78c89fddbc8f309c816
Andras <agalambos3@gmail.com>
2023-08-30 13:56:43 -0500

    rough pseudocode comments

---
+++ communication.py
@@ -0,0 +1,6 @@
+"""Class structure with communication class that will have methods for 
+receiving and sending information to arduino as well as parsing sent information. Important methods:
+checkSetup() - checks if arduino is set-up and ready to communicate
+checkCoord() - checks if arduino is ready for coordinate
+SendCoord() - sends coordinate pair to arduino to execute move
+"""
