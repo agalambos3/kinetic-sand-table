d23a095003c04210a98f8efb9e1aa46ec3aa20ef
Andras <agalambos3@gmail.com>
2023-09-03 14:30:12 -0500

    added build

--- Arduino/communication/communication.ino
+++ Arduino/communication/communication.ino
@@ -22,16 +22,21 @@
   https://www.arduino.cc/en/Tutorial/BuiltInExamples/Blink
 */
 
+#include <elapsedMillis.h>
+#include <AccelStepper.h>
+
+
 // the setup function runs once when you press reset or power the board
 void setup() {
   // initialize digital pin LED_BUILTIN as an output.
   pinMode(LED_BUILTIN, OUTPUT);
 }
 
+
 // the loop function runs over and over again forever
 void loop() {
   digitalWrite(LED_BUILTIN, HIGH);  // turn the LED on (HIGH is the voltage level)
-  delay(100);                      // wait for a second
+  delay(1000);                      // wait for a second
   digitalWrite(LED_BUILTIN, LOW);   // turn the LED off by making the voltage LOW
-  delay(20);                      // wait for a second
+  delay(1000);                      // wait for a second
 }
