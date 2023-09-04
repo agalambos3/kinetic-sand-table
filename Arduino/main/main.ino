3c941687c27f3bec4ea0c26903b50bdbc3083ba8
Andras <agalambos3@gmail.com>
2023-09-04 11:48:25 -0500

    initial commit

---
+++ Arduino/main/main.ino
@@ -0,0 +1,3 @@
+/*Main document for running arduino. For now I will put all functions in one big ugly docuement. TODO: once commuication
+is working split up stuff into different .cpp files so that it is easier to keep track of everything*/
+
