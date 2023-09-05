7e1dd446e84ef6313ace54b94bf850296f289f75
Andras <agalambos3@gmail.com>
2023-09-04 21:11:02 -0500

    includes communication.h

--- Arduino/main/main.ino
+++ Arduino/main/main.ino
@@ -1,3 +1,20 @@
 /*Main document for running arduino. For now I will put all functions in one big ugly docuement. TODO: once commuication
 is working split up stuff into different .cpp files so that it is easier to keep track of everything*/
 
+#include "communication.h"
+
+void setup() {
+  //do setup here
+  Serial.begin(115200);
+  //communicate that set-up is succesful
+  Serial.write("s");
+  recieve_ready = true;
+}
+
+void loop() {
+  recvWithStartEndMarkers();
+  if(newCommand==true){
+    Serial.print("I recieved: ");
+    Serial.println(receivedChars);
+  }
+}
