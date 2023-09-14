c74814933403083b8215db01a6dcdc208028d526
Andras <agalambos3@gmail.com>
2023-09-13 19:25:16 -0500

    comments

--- Arduino/main/communication.cpp
+++ Arduino/main/communication.cpp
@@ -1,6 +1,13 @@
 #include "communication.h"
 #include <Arduino.h>
 
+// TODO: rewrite this to more class based so that its usage in main.ino is easy to follow and makes sense
+
+/*Class should do the following:
+read command - should function like recWithStartEndMarkers() but should have stuff like recieved command passed to it
+ 
+*/
+
 bool recieve_ready = false ; //whether table is ready to receive over serial
 char receivedChars[numChars]; //array for storing recieved command 
 bool newCommand = false;//whether there is new command that is ready to be parsed and executed
