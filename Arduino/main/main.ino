0e9e9ee109518970a70b2f353c0060d6312ec54d
Andras <agalambos3@gmail.com>
2023-09-13 19:25:06 -0500

    comments

--- Arduino/main/main.ino
+++ Arduino/main/main.ino
@@ -2,13 +2,22 @@
 is working split up stuff into different .cpp files so that it is easier to keep track of everything*/
 
 #include "communication.h"
+ /* In main.ino I will declare global variables that influence the actions of the table:
+ long next_angular_step - how many steps angular_stepper.move() should be set to after current move is completed
+ long next_radial_step - how many steps radial_stepper.move() should be set to after current move is completed
+ bool homing - if true table will interrupt and home itself before moving on 
+ more variables corresponding to control of the lights
+ variables corresponding to pins on arduino
+ */
+
 
 void setup() {
   //do setup here
   Serial.begin(115200);
   //communicate that set-up is succesful
-  Serial.write("s");
+  Serial.write('s');
   recieve_ready = true;
+  Serial.write('r');
 }
 
 void loop() {
@@ -16,5 +25,7 @@ void loop() {
   if(newCommand==true){
     Serial.print("I recieved: ");
     Serial.println(receivedChars);
+    Serial.write('r');
+    newCommand = false;
   }
 }
