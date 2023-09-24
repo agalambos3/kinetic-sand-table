90c3838f3f5eeecd1527e3bfba0075b04502a5f0
Andras <agalambos3@gmail.com>
2023-09-24 10:59:59 -0500

    added variables and function defs

--- Arduino/test/comtest.h
+++ Arduino/test/comtest.h
@@ -2,9 +2,27 @@
 #define comtest
 
 class Arduinocomms {
-    public:
-        bool commandReady;
-    char giveChar();
+    static const int numChars = 15;
+    static const int cqsize = 5;
+    
+    bool isRecieving;
+    struct command 
+    {
+        int radial_steps;
+        int angular_steps;
+        /* data */
+    };//structure
+    
+    public: 
+        command command_queue[cqsize];
+        bool recieveReady;
+        char receivedChars[numChars];
+        bool commandReady = false;
+
+    char giveChar(); //test function that returns character
+    void setup(); //setup communication
+    void readCommand(); //read serial and stores in buffer
+    int parseCommand(); //takes command from buffer and parses it 
         
 };
 
