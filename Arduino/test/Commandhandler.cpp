b12458b326b333901c50cbe0e4f91b7a92b2fa58
Andras <agalambos3@gmail.com>
2023-10-11 15:13:49 -0500

    added parseCommand

--- Arduino/test/Commandhandler.cpp
+++ Arduino/test/Commandhandler.cpp
@@ -1,7 +1,9 @@
 #include "CommandHandler.h"
 
-int CommandHandler::setup(){
-    
+
+
+int CommandHandler::setup()
+{
 }
 
 void CommandHandler::run(){
@@ -15,8 +17,53 @@ void CommandHandler::run(){
     }
     }
      
-int CommandHandler::activateQ(){
-    
+
+int CommandHandler::parseCommmand(char bufcommand[])
+{
+    switch (bufcommand[0])
+    {
+    case 'S':
+        char stepchar_radial[5]= {};
+        char stepchar_angular[5] = {};
+
+        if (bufcommand[1] == ' '){
+            int indx = 2;
+            int radial_indx = 0;
+            while(bufcommand[indx]!= ' ' || radial_indx < 5){
+                
+                stepchar_radial[radial_indx] = bufcommand[indx];
+                indx++;
+                radial_indx++;
+            }
+            if (bufcommand[indx] == ' '){
+                indx++;
+                int angular_indx = 0;
+                while(bufcommand[indx] != ' ' || angular_indx < 5)
+                {
+                    stepchar_angular[angular_indx]= bufcommand[indx];
+                    indx++;
+                    angular_indx++;
+                }
+            }
+
+        long steps_radial = atol(stepchar_radial);
+        long steps_angular = atol(stepchar_angular);
+        stepCommand sc;
+        sc.radial_steps = steps_radial;
+        sc.angular_steps = steps_angular;
+        stepQ.push(&sc);
+        }
+        /* code */
+        break;
+
+    case 'L':
+        /*code*/
+
+        break;    
+    default:
+        break;
+    }
+    return 0;
 }
 
 bool CommandHandler::checkQ(){
