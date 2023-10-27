67d579875228cb118c16e7fd2a8a6da3b115cbb0
Andras <agalambos3@gmail.com>
2023-10-26 23:26:39 -0500

    test can read and execute led commands from serial

--- Arduino/test/test.ino
+++ Arduino/test/test.ino
@@ -10,21 +10,21 @@ void setup() {
   //do setup here
   ser.setup();
   //request command 
-  ser.requestCommand();
-  //parse command and add it to command queue
-  while(ser.commandReady!=true){
-    ser.readSerial();
-    }
-  Serial.println("ready command");
-  com.parseCommmand(ser.receivedChars);
-  stepCommand peeked;
-  com.stepQ.pop(&peeked);
-  Serial.print("peeked command is: ");
-  Serial.print(peeked.angular_steps);
-  Serial.print(" angular step, ");
-  Serial.print(peeked.radial_steps);
-  Serial.println(" radial steps");
-  while(false){};
+  // ser.requestCommand();
+  // //parse command and add it to command queue
+  // while(ser.commandReady!=true){
+  //   ser.readSerial();
+  //   }
+  // Serial.println("ready command");
+  // com.parseCommmand(ser.receivedChars);
+  // stepCommand peeked;
+  // com.stepQ.pop(&peeked);
+  // Serial.print("peeked command is: ");
+  // Serial.print(peeked.angular_steps);
+  // Serial.print(" angular step, ");
+  // Serial.print(peeked.radial_steps);
+  // Serial.println(" radial steps");
+  // while(false){};
   
   
   //at this point table should have one command it can start running right away rest will fall into place
@@ -32,16 +32,23 @@ void setup() {
 
 void loop() {
 
-  com.run(); //run active command or pop command from queue make active and then run command 
+  
+  if(com.run()==1){
+      ser.requestnum--;
+  } //run active command or pop command from queue make active and then run command
+   
 
   ser.readSerial(); //check serial port read and store to buffer 
 
-  if (com.checkQ() != true){ // check if queue is full 
-    // ser.requestCommand(); //if queue is not full request command over serial
+  if (com.checklightQ() != true && ser.requestnum <=4){ // check if queue is full 
+    ser.requestCommand(1); //if queue is not full request command over serial
   }
 
   if(ser.commandReady == true){ //check if there is command ready to be parsed from buffer
-    com.parseCommmand(ser.receivedChars); //parse command from buffer and add it to command queue
+    
+    if(com.parseCommmand(ser.receivedChars)== 1){
+      Serial.println("command parsed");
+    } //parse command from buffer and add it to command queue
     ser.commandReady = false;
   }
 }
