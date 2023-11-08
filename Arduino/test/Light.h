213180b26f14634877a57b93dc8d94ad040b9b22
Andras <agalambos3@gmail.com>
2023-11-07 22:27:04 -0600

    variable scoping

--- Arduino/test/Light.h
+++ Arduino/test/Light.h
@@ -6,10 +6,10 @@
 #include "elapsedMillis.h"
 
 class Light{
-    
+    long duration;
     public:
     Light(long clength);
-    long duration;
+    
     elapsedMillis lightElapsed;
     int run(); //called every loop if there is active command
     int set(long command_length); // called if there is no active command so parameters need to be set 
