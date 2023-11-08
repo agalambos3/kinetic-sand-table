263351bfdae4c6924795a959397b6171be1b81eb
Andras <agalambos3@gmail.com>
2023-11-07 21:56:32 -0600

    added set() and run() method

--- Arduino/test/Stepper.cpp
+++ Arduino/test/Stepper.cpp
@@ -45,9 +45,61 @@ int Steppers::setup(){
 }
 
 int Steppers::set(long angular,long radial){
+    if (angular > 0)
+    {
+        angularDriver.shaft(true);
+    }
+    else{
+        angularDriver.shaft(false);
+    }
+    if (radial > 0)
+    {
+        radialDriver.shaft(true);
+    }
+    else{
+        radialDriver.shaft(false);
+    }
+    if (angular>radial)
+    {
+        angular_step_interval = ANGULAR__TIME_PER_MICROSTEP;
+        long move_time = angular_step_interval*angular;
+        radial_step_interval = move_time/radial;
+
+    }
+    else{
+        radial_step_interval = RADIAL__TIME_PER_MICROSTEP;
+        long move_time = radial_step_interval*radial;
+        angular_step_interval = move_time/angular;
+    }
+    
     angular_steps = angular;
     radial_steps = radial;
-
+    radial_elapsed_time = 0;
+    angular_elapsed_time = 0;
     return 1;
 
+}
+
+int Steppers::run(){
+    if(angular_steps > 0 || radial_steps > 0){
+        if (angular_steps>0 && angular_elapsed_time > angular_step_interval)
+        {
+            digitalWrite(ANGULAR_STEP_PIN,HIGH);
+            digitalWrite(ANGULAR_STEP_PIN,LOW);
+            angular_steps--;
+            angular_elapsed_time -= angular_step_interval;
+        }
+        if (radial_steps> 0 && radial_elapsed_time > radial_step_interval)
+        {
+            digitalWrite(RADIAL_STEP_PIN,HIGH);
+            digitalWrite(RADIAL_STEP_PIN,LOW);
+            radial_steps--;
+            radial_elapsed_time -= radial_step_interval;
+
+        }
+        
+    } 
+    else{
+        return 0; // command is over return letting commandhandler know that this is the case 
+    }
 }
