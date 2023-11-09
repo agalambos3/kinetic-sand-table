ddd3e73f962387c375264e0e143ab0f2116b9a94
Andras <agalambos3@gmail.com>
2023-11-08 19:25:44 -0600

    commented out parseCommand()

--- Arduino/test/SerialHandler.cpp
+++ Arduino/test/SerialHandler.cpp
@@ -48,46 +48,46 @@ void SerialHandler::readSerial(){
 
 
 // TODO move over to command handler 
-void SerialHandler::parseCommand(char bufcommand[]){
-    // TODO: rewrite with switch syntax because it will be easier to follow and understand
-    if (bufcommand[0] == 'S'){
-        char stepchar_radial[5]= {};
-        char stepchar_angular[5] = {};
-        long steps_radial;
-        long steps_angular;
-        if (bufcommand[1] == ' '){
-            int indx = 2;
-            int rindx = 0;
-            while(bufcommand[indx]!= ' ' || rindx < 5){
+// void SerialHandler::parseCommand(char bufcommand[]){
+//     // TODO: rewrite with switch syntax because it will be easier to follow and understand
+//     if (bufcommand[0] == 'S'){
+//         char stepchar_radial[6]= {};
+//         char stepchar_angular[6] = {};
+//         long steps_radial;
+//         long steps_angular;
+//         if (bufcommand[1] == ' '){
+//             int indx = 2;
+//             int rindx = 0;
+//             while(bufcommand[indx]!= ' ' && rindx < 6){
                 
-                stepchar_radial[rindx] = bufcommand[indx];
-                indx++;
-                rindx++;
-            }
-            if (bufcommand[indx] == ' '){
-                indx++;
-                int aindx = 0;
-                while(bufcommand[indx] != ' ' || aindx < 5)
-                {
-                    stepchar_angular[aindx]= bufcommand[indx];
-                    indx++;
-                    aindx++;
-                }
-            }
-        steps_radial = atol(stepchar_radial);
-        steps_angular = atol(stepchar_angular);
-        }
-        Serial.println(stepchar_radial);
-        Serial.println(stepchar_angular);
-        Serial.println(steps_radial);
-        Serial.println(steps_angular);
-        // Serial.write("Step command recieved. Radial move is");
-        // Serial.write(steps_radial);
-        // Serial.write(" step");
+//                 stepchar_radial[rindx] = bufcommand[indx];
+//                 indx++;
+//                 rindx++;
+//             }
+//             if (bufcommand[indx] == ' '){
+//                 indx++;
+//                 int aindx = 0;
+//                 while(bufcommand[indx] != ' ' && aindx < 6)
+//                 {
+//                     stepchar_angular[aindx]= bufcommand[indx];
+//                     indx++;
+//                     aindx++;
+//                 }
+//             }
+//         steps_radial = atol(stepchar_radial);
+//         steps_angular = atol(stepchar_angular);
+//         }
+//         Serial.println(stepchar_radial);
+//         Serial.println(stepchar_angular);
+//         Serial.println(steps_radial);
+//         Serial.println(steps_angular);
+//         // Serial.write("Step command recieved. Radial move is");
+//         // Serial.write(steps_radial);
+//         // Serial.write(" step");
 
-    }
+//     }
 
-}
+// }
 
 void SerialHandler::requestCommand(int command_type){
     requestnum++;
