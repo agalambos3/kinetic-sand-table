14803c3f73a85b967737134a25f2ff2e6a85d92f
Andras <agalambos3@gmail.com>
2024-01-21 13:39:13 -0600

    added stallguard setup lines

--- Arduino/test/Stepper.cpp
+++ Arduino/test/Stepper.cpp
@@ -2,6 +2,9 @@
 
 int Steppers::setup(){
 
+    // initialize HW UART
+    STEPPER_SERIAL_PORT.begin(115200);
+
     //  setup pins for angular driver
     pinMode(ANGULAR_STEP_PIN,OUTPUT);
     pinMode(ANGULAR_DIR_PIN,OUTPUT);
@@ -10,17 +13,15 @@ int Steppers::setup(){
     //enable angular driver in hardware
     digitalWrite(ANGULAR_EN_PIN,LOW); 
 
-    // intialize HW UART and angular driver 
-    ANGULAR_SERIAL_PORT.begin(115200);
+    //intialize angular driver 
     angularDriver.begin();
 
     // UART settings for angular driver
     angularDriver.toff(5);
     angularDriver.rms_current(400);
-    angularDriver.microsteps(16);
+    angularDriver.microsteps(ANGULAR_MICROSTEPS);
     angularDriver.en_spreadCycle(false);
     angularDriver.pwm_autoscale(true);
-    angularDriver.SGTHRS(STALL_VALUE);
 
     // setup pins for radial driver
     pinMode(RADIAL_STEP_PIN,OUTPUT);
@@ -30,14 +31,13 @@ int Steppers::setup(){
     //enable radial driver in hardware
     digitalWrite(RADIAL_EN_PIN,LOW);
 
-    // intialize HW UART and angular driver
-    RADIAL_SERIAL_PORT.begin(115200);
+    // intialize radial driver
     radialDriver.begin();
 
-    // UART settings for angular driver
-    radialDriver.toff(5);
+    // UART settings for radial driver
+    radialDriver.toff(5); //TODO: check what this value should be to get consistent stallguard results
     radialDriver.rms_current(400);
-    radialDriver.microsteps(16);
+    radialDriver.microsteps(RADIAL_MICROSTEPS);
     radialDriver.en_spreadCycle(false);
     radialDriver.pwm_autoscale(true);
     radialDriver.SGTHRS(STALL_VALUE); //setting threshold for stall guard
