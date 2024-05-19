1d5357e00170c3ae152eec682bdfd72c5d07c27b
Andras <agalambos3@gmail.com>
2024-05-19 17:45:04 -0500

    rearranging of thr files

--- main.py
+++ main.py
@@ -2,6 +2,21 @@
 
 
 import serial
+import os 
+import random
+
+#directory for thr file
+thrDirectory = "dithermaster_thr"
+isSketchActive = False
+# directory of active sketch
+activeSketchDir = None
+#file for active sketch
+activeSketch = None
+
+
+
+
+
 
 # setup function to run and setup serial connection
 
@@ -9,19 +24,50 @@ def setup():
     """setup function that runs when script is first called"""
     # serial port 
     ser = serial.Serial('/dev/cu.usbmodem101',baudrate=115200,timeout=.1)
-    
-    return 0
+    while(ser.is_open==False):
+        # wait until serial port is open
+        pass
+
+    # read until arduino ready signal is sent
+    ser.read_until(b'R')
 
 
 def loop():
     '''loop that runs forever managing table'''
     while True:
-        # do stuff forever
-        return None
+        if(isSketchActive == True):
+            #
+            pass
+
+
+        # if sketch is running
     
+        # do stuff forever
+        
+        else:
+            random.choice(os.listdir(thrDirectory))
+            pass
 
-setup()
-loop()
+
+    
+activeSketchName = random.choice(os.listdir(thrDirectory))
+activeSketch=open(thrDirectory+"/"+activeSketchName)
+print(activeSketch.readline())
+# setup()
+# loop()
 
 
+'''What needs to happen in main loop:
+- check serial port for command request
+- if command requested
+    - if command qd (and matches requested command) send commmand 
+    - else q command and send 
+- else
+    - q command 
 
+Program also has to have:
+- way to randomly select thr file
+- parse thr one line at a time to q commands
+
+
+'''
