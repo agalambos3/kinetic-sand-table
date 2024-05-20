697b4b2555fd866ab8ec28c8daebd169e92cb597
Andras <agalambos3@gmail.com>
2024-05-20 14:28:04 -0500

    commenting and cleaning up

--- Arduino/mainV2/Stepper.cpp
+++ Arduino/mainV2/Stepper.cpp
@@ -45,11 +45,6 @@ int StepperHandler::setup(){
     
     commandDone = false;
 
-    // //  set up timer for radial stepper
-    // TCCR3A = 0;           // Init Timer3
-    // TCCR3B = 0;           // Init Timer3
-    // TCCR3B |= (1 << CS10); //set Timer3 prescalar to 1
-    // OCR3A = radialTimerTout;        // Timer3 CompareA Register
     return 1;
 }
 
@@ -82,18 +77,16 @@ void StepperHandler::beginCommand(stepCommand* ptr){
     OCR3A = activeCommand.radialTimerCount;   // Timer3 CompareA Register
     TIMSK3 |= (1 << OCIE1A);  // Enable Timer3 COMPA Interrupt
     
+    //setup angular timer 
     TCCR1A = 0; // Init Timer4
     TCCR1B = 0; // Init Timer4
     TCCR1B |= (1 << CS10); //set Timer4 prescalar to 1
     OCR1A = activeCommand.angularTimerCount; //Timer4 CompareA Register
-    TIMSK1 |= (1 << OCIE1A); //  Enable Timer4 COMPA Interrupt
-    //TODO: check if CompareB register works for angular way I think it does
-
-    
+    TIMSK1 |= (1 << OCIE1A); //  Enable Timer4 COMPA Interrupt    
 }
 
+//ISR for radial stepper movement
 void StepperHandler::radialStepISR(){
-  //make this section work nicer
   if(radialSteps<abs(activeCommand.radialStepGoal)){
     digitalWrite(RADIAL_STEP_PIN, HIGH);
     digitalWrite(RADIAL_STEP_PIN, LOW);
@@ -111,6 +104,7 @@ void StepperHandler::radialStepISR(){
   }
 }
 
+//ISR for angular stepper movement
 void StepperHandler::angularStepISR(){
   if(angularSteps<abs(activeCommand.angularStepGoal)){
     digitalWrite(ANGULAR_STEP_PIN, HIGH);
@@ -131,22 +125,3 @@ void StepperHandler::angularStepISR(){
 bool StepperHandler::isCommandDone(){
   return commandDone;
 }
-
-
-// void StepperHandler::stepISR(){
-//   if(&radialSteps < &activeCommand.radialStepGoal){
-//     digitalWrite(RADIAL_STEP_PIN, HIGH);
-//     digitalWrite(RADIAL_STEP_PIN, LOW);
-//     OCR3A += activeCommand.radialTimerCount; 
-//     radialSteps += 1;
-//   }
-//   else{
-//     TCCR3B &= (0<<CS10)|(0<<CS11)|(0 << CS12); //turn off Timer3 to stop motor stepping
-//     // StepperHandler::isCommandActive = false; //sets is command active flag to false
-//   }
-// }
-
-
-
-
-
--- Arduino/mainV2/Stepper.h
+++ Arduino/mainV2/Stepper.h
@@ -7,17 +7,14 @@
 #include "utility.h"
 
 
-
-// serial port for stepper driver 
-#define STEPPER_SERIAL_PORT Serial1
+#define STEPPER_SERIAL_PORT Serial1 // serial port for stepper driver 
 
 // pin definitions for angular stepper driver
 #define ANGULAR_EN_PIN 6 
 #define ANGULAR_DIR_PIN 5 
 #define ANGULAR_STEP_PIN 4
 
-// serial address for angular stepper driver
-#define ANGULAR_DRIVER_ADDRESS 0b00
+#define ANGULAR_DRIVER_ADDRESS 0b00 // serial address for angular stepper driver
 
 // pin defitions for radial stepper driver
 #define RADIAL_EN_PIN 9 
@@ -25,20 +22,19 @@
 #define RADIAL_STEP_PIN 7
 #define RADIAL_DIAG_PIN 3
 
-// serial address for radial stepper driver
-#define RADIAL_DRIVER_ADDRESS 0b01
+#define RADIAL_DRIVER_ADDRESS 0b01 // serial address for radial stepper driver
 
 
-// internal resistor setting for TMC2209 so that current can be controlled through UART
-#define R_SENSE 0.11f
-// rms current for steppers (in milliamps)
-#define RMS_CURRENT 400
-// microstepping for angular stepper motor 
-#define MICROSTEPS 16
-// stall value at which stall guard is triggered
-#define STALL_VALUE 80
-// minimum time (in ms) between stall events (used for simple debouncing of diag pin) 
-#define MIN_STALL_TIME 100
+
+#define R_SENSE 0.11f // internal resistor setting for stepper driver (TMC 2209)
+#define RMS_CURRENT 400 // rms current for steppers (in milliamps)
+#define MICROSTEPS 16 // microstepping for angular stepper motor 
+
+
+#define DIAG_PIN 3 //diag pin used for stallguard interrupt
+#define STALL_VALUE 4 // stall value at which stall guard is triggered
+#define MIN_STALL_TIME 500 // minimum time (in ms) between stall events (used for simple debouncing of diag pin) 
+
 
 
 
